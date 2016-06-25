8bb6bf6bd796930b293818468fd2758a9d35b249
Juan Ricardo Enriquez Torres <jrichiemx@Juans-MacBook-Pro.local>
2016-06-25 09:00:45 -0400

    add swagger

--- app/HolaController.java
+++ app/HolaController.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import play.mvc.Controller;
 import play.mvc.Result;
 /**
  * Created by jrichiemx on 6/23/16.
  */
+
 public class HolaController extends Controller {
+
 
     public Result sayHola() {
         return ok("It works!");
--- build.sbt
+++ build.sbt
@@ -5,4 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
-    
+
+libraryDependencies ++= Seq(
+  "io.swagger" %% "swagger-play2" % "1.5.1"
+)
+
